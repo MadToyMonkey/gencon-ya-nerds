0c46323358cc68c34f7e9bdc30723bd2dbb19471
Madz Derringer <madderringer@gmail.com>
2024-07-18 19:34:15 -0500

    EMAIL SENT

--- src/email_gen.py
+++ src/email_gen.py
@@ -9,20 +9,31 @@ import smtplib
 from email.message import EmailMessage
 
 # email account info
-email_address = "shitlords21@gmail.com"
+email_address = "dungeonmasterBU@hotmail.com"
 email_password = "12ampcurrent"
-smtp_server = "smtp.gmail.com"
+smtp_server = "smtp-mail.outlook.com"
 smtp_port = 587
 
 
-def task_email():
+def task_email(r_email, name, tasks):
     """creating the emails to send"""
     msg = EmailMessage()
+    msg["Subject"] = "GENCON TASKS"
     msg["From"] = email_address
-    msg["To"] = "recipient_email@example.com"
-    msg.set_content("Hello Gilfoyle, this is a test email with an attachment. -Dinesh")
-    with open("attachment.txt", "rb") as f:
-        file_data = f.read()
-    msg.add_attachment(
-        file_data, maintype="text", subtype="plain", filename="attachment.txt"
+    msg["To"] = r_email
+    msg.set_content(
+        f"Hello {name}, here are your tasks for the day.\n {tasks}\n\n Have a nice day!"
     )
+    # with open("attachment.txt", "rb") as f:
+    #    file_data = f.read()
+    # msg.add_attachment(
+    #    file_data, maintype="text", subtype="plain", filename="attachment.txt"
+    # )
+    # Send email message
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(email_address, email_password)
+        server.send_message(msg)
+
+
+# task_email("sochjr@gmail.com", "Josh", "DM Madz you received this message.")
